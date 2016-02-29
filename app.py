7129aaf1b5024dd5fec2d0365bf0fa46b4f7639e
jeseon <jeseon@gmail.com>
2016-02-29 20:24:21 +0900

    Adding template

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 from flask import Flask
+from flask_admin import Admin
 from flask.ext.sqlalchemy import SQLAlchemy
+from views import CeoNoticeView
 import os
 import config
 
@@ -20,6 +22,8 @@ def hello():
 def notice(id):
     return 'Title: ' + CeoNotice.query.filter_by(id=id).first().title
 
+admin = Admin(app, name='Flask-Admin', template_mode='bootstrap3')
+admin.add_view(CeoNoticeView(CeoNotice, db.session, category='공지사항', name='점주 공지사항'))
 
 if __name__ == '__main__':
 	app.run()
